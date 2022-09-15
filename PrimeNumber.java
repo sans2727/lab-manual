71f46607d064d235296a2adca94eb0e4fb4fff22
sans2727 <112940508+sans2727@users.noreply.github.com>
2022-09-15 15:43:54 +0530

    Add files via upload

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,22 +1,24 @@
-class PrimeNumber
-{
-  public static void main(String[] args) 
-  {
-    java.util.Scanner sc = new java.util.Scanner(System.in);
-    System.out.println("Enter a no: ");
-    int num = sc.nextInt();
-    boolean flag = false;
-    for (int i = 2; i <= num / 2; ++i) 
-    {
-      if (num % i == 0) 
-      {
-        flag = true;
-       break;
-     }
+public class PrimeNumber {
+ 
+    public static void main(String[] args) {
+        //range
+        int min = 2;
+        int max = 100;
+        //find all prime numbers in the given range
+        for(int n=min;n<=max;n++) {
+            //check if this number is prime
+            if(isPrime(n)) {
+                System.out.println(n);
+            }
+        }
     }
-    if (!flag)
-      System.out.println(num + " is a prime number.");
-    else
-      System.out.println(num + " is not a prime number.");
-  }
-} 
+     
+    public static boolean isPrime(int num) {    
+        for(int i = 2; i <= num/i; ++i) {
+            if(num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
