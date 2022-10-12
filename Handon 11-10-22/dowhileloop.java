dbc09352627505fd205927e8a5fa5c8d04f16f83
sans2727 <112940508+sans2727@users.noreply.github.com>
2022-10-12 11:59:42 +0530

    Add files via upload

---
+++ Handon 11-10-22/dowhileloop.java
@@ -0,0 +1,24 @@
+class DoWhileLoop {
+
+	public static void main(String[] args)
+	{
+		int num,i,reversed = 30;
+	    // taking values from user at run time
+	   Scanner sc = new Scanner(System.in);
+	   System.out.println("Enter the any Numbers: ");
+	   num = sc.nextInt();
+	   
+	   sc.close();
+	   //condition for printing reverse of given numbers
+	   do{
+		    i=num%10;
+		    reversed=reversed*10+i;
+		    num=num/10;
+		}
+	   while(num>30);
+		
+		System.out.println("Reverse of the number is: "+reversed);
+	   
+	}
+
+}
