5724e6781dae54f6a22c3bf4a18213565df5f2d0
sans2727 <112940508+sans2727@users.noreply.github.com>
2022-10-13 13:49:14 +0530

    Add files via upload

---
+++ 13-10-22/whileloop.java
@@ -0,0 +1,22 @@
+public class whileloop {
+
+	public static void main(String[] args) 
+	{
+		int num = 10;
+		
+		
+		int sum=0,i=1;
+		
+		while(i<=num)
+			{
+			sum=sum+i;
+			i++;
+			
+			}
+		System.out.println("sum of 1-10 natural numbers:-" +sum);
+
+
+
+	}
+
+}
