4fe9f807be03f31907093bf665993fc4bd884aa4
sans2727 <112940508+sans2727@users.noreply.github.com>
2022-10-27 12:04:18 +0530

    Add files via upload

---
+++ 27-10-22/Happy diwali.java
@@ -0,0 +1,15 @@
+public class Happy_Diwali {
+
+public static void main(String[] args) {
+
+String d = "HAPPY DIWALI";//declaring the string 
+
+for (int i=0;i<d.length();i++)//using for loop for printing the string
+{
+
+System.out.println(d);
+break;//break statement to exit the loop after printing the string
+}
+}
+
+}
